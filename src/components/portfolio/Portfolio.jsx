8775044ce5cfae5b672314d05f27bfc8f1529d53
d3oxy <ishansachu1@gmail.com>
2022-07-14 08:17:11 +0000

    added function to disable certain buttions

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,56 +1,51 @@
 import React from "react";
 import "./portfolio.css";
-import IMG1 from "../../assets/personal-portfolio-thumbnail.png";
-import IMG2 from "../../assets/portfolio2.jpg";
-import IMG3 from "../../assets/portfolio3.jpg";
-import IMG4 from "../../assets/portfolio4.jpg";
-import IMG5 from "../../assets/portfolio5.png";
-// import IMG6 from '../../assets/portfolio6.jpg'
+
 const data = [
   {
     id: 1,
-    image: IMG1,
+    image: "https://i.postimg.cc/nz97yDBV/screely-1657783601718.png",
     title: "My new Personal Website.",
-    github: "https://github.com",
-    gitEnabled: false,
-    demo: "https://deoxy.tech",
+    github: "https://github.com/D3OXY/react-personal-website",
+    gitEnabled: true,
+    demo: "https://new.deoxy.tech",
     demoEnabled: true,
   },
   {
     id: 2,
     image: IMG2,
-    title: "A Browser Based Chat App.",
-    github: "https://github.com",
-    gitEnabled: false,
-    demo: "https://deoxy.tech",
+    title: "Browser Based Tetris Game.",
+    github: "https://github.com/D3OXY/tetris",
+    gitEnabled: true,
+    demo: "https://d3oxy.github.io/tetris/",
     demoEnabled: true,
   },
   {
     id: 3,
     image: IMG3,
-    title: "Admin Dashboard Made Using MERN Stack.",
+    title: "Temp Placeholder",
     github: "https://github.com",
-    gitEnabled: true,
+    gitEnabled: false,
     demo: "https://deoxy.tech",
     demoEnabled: false,
   },
   {
     id: 4,
     image: IMG4,
-    title: "Simple React Authentication Using Firebase.",
+    title: "Temp Placeholder",
     github: "https://github.com",
-    gitEnabled: true,
-    demo: "https://react-auth.demo.deoxy.tech",
-    demoEnabled: true,
+    gitEnabled: false,
+    demo: "https://deoxy.tech",
+    demoEnabled: false,
   },
   {
     id: 5,
     image: IMG5,
-    title: "A Browser based Tetris Games using HTML, CSS and Javascript",
-    github: "https://github.com/D3OXY/tetris",
-    gitEnabled: true,
-    demo: "https://tetris.demo.deoxy.tech",
-    demoEnabled: true,
+    title: "Temp Placeholder",
+    github: "https://github.com",
+    gitEnabled: false,
+    demo: "https://deoxy.tech",
+    demoEnabled: false,
   },
 ];
 function Portfolio() {
@@ -68,46 +63,8 @@ function Portfolio() {
                 </div>
                 <h3>{title}</h3>
                 <div className="portfolio__item-cta">
-                  {gitEnabled ? (
-                    <a
-                      href={github}
-                      className="btn"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Github
-                    </a>
-                  ) : (
-                    <a
-                      href={github}
-                      active
-                      className="btn"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Github
-                    </a>
-                  )}
-                  {demoEnabled ? (
-                    <a
-                      href={demo}
-                      className="btn btn-primary"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Live Demo
-                    </a>
-                  ) : (
-                    <a
-                      href={demo}
-                      active
-                      className="btn btn-primary"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Live Demo
-                    </a>
-                  )}
+                  <a href={github} className={gitEnabled ? "btn" : "btn disabled-link"} target="_blank" rel="noreferrer">Github</a>
+                  <a href={demo} className={demoEnabled ? "btn btn-primary" : "btn btn-primary disabled-link"} target="_blank" rel="noreferrer">Live Demo</a>
                 </div>
               </article>
             );
