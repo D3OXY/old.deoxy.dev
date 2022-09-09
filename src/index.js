451a0f5043b769fcc791cf3efe75798aae29a54f
d3oxy <ishansachu1@gmail.com>
2022-09-09 12:04:21 +0000

    updated render to react 18

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import App from "./App";
 import './index.css';
-ReactDOM.render(<App />, document.querySelector("#root"));
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<App tab="home" />);
