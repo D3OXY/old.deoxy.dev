1304c30454c2143fe2d66d91d50a8aa07c2f14d7
d3oxy <ishansachu1@gmail.com>
2023-03-27 08:10:25 +0000

    add: review

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -10,27 +10,29 @@ import noimagemale from '../../assets/noimagemale.jpeg'
 // import noimagefemale from '../../assets/noimagefemale.jpg'
 import geoffdesigns from '../../assets/geoffdesigns.webp'
 import farah03 from '../../assets/farah03.webp'
+import sam678740 from '../../assets/sam678740.webp'
 const data = [
     {
-        id: 1,
         avatar: farah03,
         name: 'Farah03',
         review: 'Wonderful experience! Deoxy was very patient with helping me set up the bot .He was very easy to communicate with and responded immediately to all queries.Even if you are a newbie to discord ,he can set it all up for you.Very happy with his work .Will definitely work with him again!'
     },
     {
-        id: 2,
+        avatar: sam678740,
+        name: 'sam678740',
+        review: 'Very responsive and understanding. Got the job done in a very timely manner. Loved working with him and will again in the future. Thanks'
+    },
+    {
         avatar: geoffdesigns,
         name: 'geoffdesigns',
         review: 'Ishan is very helpful and skilled! Extremely happy with the discord bot! Thank you!'
     },
     {
-        id: 3,
         avatar: noimagemale,
         name: 'J8TIN',
         review: 'Deoxy was such a great help setting up my Whmcs page Thankyou so much'
     },
     {
-        id: 4,
         avatar: noimagemale,
         name: 'deejay616',
         review: 'This guy was the best! He even give me advice. I am really looking forward to work with him in the future 100% satisfied'
@@ -48,9 +50,9 @@ function Testimonials() {
                 pagination={{ clickable: true }}
             >
                 {
-                    data.map(({ id, avatar, name, review }, index) => {
+                    data.map(({ avatar, name, review }, index) => {
                         return (
-                            <SwiperSlide key={id} className='testimonial'>
+                            <SwiperSlide key={index} className='testimonial'>
                                 <div className="client__avatar">
                                     <img src={avatar} alt="" />
                                 </div>
